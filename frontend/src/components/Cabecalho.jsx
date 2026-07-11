a95646fbb4aa211e6b26640fbbe5743ed19794f0
Edson Silva <negoedd@gmail.com>
2026-07-11 01:23:08 -0300

    Criado o componente do header

---
+++ frontend/src/components/Cabecalho.jsx
@@ -0,0 +1,11 @@
+export default function Cabecalho() {
+  return (
+    <header className="cabecalho">
+      <h1>AtendeOn</h1>
+      <p>
+        A solução digital inteligente para o controle de mesas e fechamento de
+        contas em tempo real.
+      </p>
+    </header>
+  );
+}
